dd8ea0e725b487592e4548018fb62ab4e8531053
AwesomeDemoGuy <sebastian11ashkar@protonmail.com>
2023-10-19 17:22:57 -0700

    worked on implementing the collide3d library

--- src/main.menu.js
+++ src/main.menu.js
@@ -1,9 +1,12 @@
 
 
 function setup() {
+    createCanvas(400, 400);
+    myButton = new Clickable(200, 200);
 
 }
 
 function draw() {
+    background(200);
 
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,43 @@
+var click1;
+
+shape1 = [];
+var hit = false;
+function preload() {
+  shape1[0] = createVector(75, 200);
+  shape1[1] = createVector(125, 100);
+  shape1[2] = createVector(225, 100);
+  shape1[3] = createVector(175, 200);
+}
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  myButton = new Clickable();     //Create button
+  myButton.locate(20, 20);        //Position Button
+  myButton.onPress = function(){  //When myButton is pressed
+  this.color = "#AAAAFF";       //Change button color
+  alert("Yay!");                //Show an alert message
+  
+}
+
   }
   
-  function draw() {
-    background(220);
-    square(255,255,255);
-  }
+function draw() {
+  //
+  background(120);
+  
+  beginShape();
+  for (const { x, y } of shape1)  vertex(x, y);
+  endShape(CLOSE);
+
+  circle(mouseX, mouseY, 10);
+
+  //hit = collidePointPoly(mouseX, mouseY, shape1);
+  //hit = collidePointPoint(100, 100, 100, 100);
+  console.log("hit");
+  
+  //quad(75, 200, 125, 100, 225, 100, 175, 200); 
+  //square(155,155,155);
+  myButton.draw();  
+  
+}
+
