a670f61af1295e618d2bc40693167d61e5330a53
AwesomeDemoGuy <sebastian11ashkar@protonmail.com>
2023-10-19 18:15:25 -0700

    added dragging to shapes

--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,23 @@
 var click1;
-
-shape1 = [];
-var hit = false;
+// The movable parallelogram
+shape1a = [];
+// The destination parallelogram
+shape1b = [];
+var hit, mouseDiff = false;
 function preload() {
-  shape1[0] = createVector(75, 200);
-  shape1[1] = createVector(125, 100);
-  shape1[2] = createVector(225, 100);
-  shape1[3] = createVector(175, 200);
+  // Define initial position for movable parallelogram
+  shape1a[0] = createVector(50, 200);
+  shape1a[1] = createVector(100, 100);
+  shape1a[2] = createVector(200, 100);
+  shape1a[3] = createVector(150, 200);
+  
+  // Define position for destination parallelogram
+  shape1b[0] = createVector(425, 500);
+  shape1b[1] = createVector(475, 400);
+  shape1b[2] = createVector(575, 400);
+  shape1b[3] = createVector(525, 500);
+
+  mouseDiff = createVector(); // temp vec for randomPoly[]
 }
 function setup() {
   createCanvas(600, 600);
@@ -25,19 +36,30 @@ function draw() {
   //
   background(120);
   
+  // Draw the destination parallelogram
   beginShape();
-  for (const { x, y } of shape1)  vertex(x, y);
+  for (const { x, y } of shape1b)  vertex(x, y);
+  endShape(CLOSE);
+  
+  // Draw the movable parallelogram
+  beginShape();
+  for (const { x, y } of shape1a)  vertex(x, y);
   endShape(CLOSE);
 
   circle(mouseX, mouseY, 10);
 
-  hit = collidePointPoly(mouseX, mouseY, shape1);
+  hit = collidePointPoly(mouseX, mouseY, shape1a);
   //hit = collidePointPoint(100, 100, 100, 100);
-  if(hit) {
-    console.log("Hit!")
+  
+  if (hit && mouseIsPressed) {
+    shape1a[0].set(mouseX -75, mouseY + 50);
+    shape1a[1].set(mouseX - 25, mouseY - 50);
+    shape1a[2].set(mouseX + 75, mouseY - 50);
+    shape1a[3].set(mouseX + 25, mouseY + 50);
+    
   }
   
-  //quad(75, 200, 125, 100, 225, 100, 175, 200); 
+  //quad(75, 200, 125, 100, 225, 100, 175, 200);
   //square(155,155,155);
   myButton.draw();  
   
