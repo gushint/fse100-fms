76847c8e9728b85c932bc2c57ab7de8d09df46f7
Gus <51027323+gushint@users.noreply.github.com>
2023-10-25 18:52:19 -0700

    Revert

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -34,9 +34,9 @@ function credits() {
 }
   
 function toShapeSort() {
-    window.open('shape_sort.html')
+    window.open('/pages/shape_sort.html')
 }
 
 function toTyping() {
-  window.open('typing.html');
+  window.open('/pages/typing.html');
 }
