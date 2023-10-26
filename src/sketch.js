e3ee5b52462d42ef2a3c3cd65cf34b6eed1f5474
AwesomeDemoGuy <sebastian11ashkar@protonmail.com>
2023-10-25 19:05:41 -0700

    color sort game now has movable shapes

--- src/sketch.js
+++ src/sketch.js
@@ -1,63 +1,69 @@
-var click1;
 // The movable parallelogram
-shape1a = [];
+shape1g = [];
 // The destination parallelogram
 shape1b = [];
 
 // The movable triangle
-shape3a = [];
+shape3g = [];
 // The destination triangle
 shape3b = [];
 
 // The movable diamond
-shape4a = [];
+shape4g = [];
 // The destination diamond
 shape4b = [];
 
-var hit, mouseDiff = false;
-
 function preload() {
-  // Define initial position for movable parallelogram
-  shape1a[0] = createVector(50, 200);
-  shape1a[1] = createVector(100, 100);
-  shape1a[2] = createVector(200, 100);
-  shape1a[3] = createVector(150, 200);
-  
-  // Define position for destination parallelogram
-  shape1b[0] = createVector(425, 500);
-  shape1b[1] = createVector(475, 400);
-  shape1b[2] = createVector(575, 400);
-  shape1b[3] = createVector(525, 500);
+  // Define the initial position for green parallelogram
+  shape1g[0] = createVector(50, 650);
+  shape1g[1] = createVector(100, 550);
+  shape1g[2] = createVector(200, 550);
+  shape1g[3] = createVector(150, 650);
+
+  // Define the initial position for the blue parallelogram
+  shape1b[0] = createVector(550, 650);
+  shape1b[1] = createVector(600, 550);
+  shape1b[2] = createVector(700, 550);
+  shape1b[3] = createVector(650, 650);
 
   mouseDiff = createVector(); // temp vec for randomPoly[]
 
-  circleX = 100;
-  circleY = 300;
+  // Define initial position for the green circle
+  circleXg = 780;
+  circleYg = 600;
+  
+  // Define initial position for the blue circle
+  circleXb = 550;
+  circleYb = 500;
   
   
-  // Define initial position for the movable triangle
-  shape3a[0] = createVector(50, 500);
-  shape3a[1] = createVector(100, 400);
-  shape3a[2] = createVector(150, 500);
-  // Define position for destination triangle
-  shape3b[0] = createVector(450, 350);
-  shape3b[1] = createVector(500, 250);
-  shape3b[2] = createVector(550, 350);
+  // Define initial position for the green triangle
+  shape3g[0] = createVector(250, 500);
+  shape3g[1] = createVector(300, 400);
+  shape3g[2] = createVector(350, 500);
 
-  // Define initial position for the movable diamond
-  shape4a[0] = createVector(50, 600);
-  shape4a[1] = createVector(100, 525);
-  shape4a[2] = createVector(150, 600);
-  shape4a[3] = createVector(100, 675);
+  // Define initial position for the blue triangle
+  shape3b[0] = createVector(250, 600);
+  shape3b[1] = createVector(300, 500);
+  shape3b[2] = createVector(350, 600);
 
-  // Define position for destination diamond
-  shape4b[0] = createVector(450, 150);
-  shape4b[1] = createVector(500, 75);
-  shape4b[2] = createVector(550, 150);
-  shape4b[3] = createVector(500, 225);
+  // Define initial position for the green diamond
+  shape4g[0] = createVector(400, 600);
+  shape4g[1] = createVector(450, 525);
+  shape4g[2] = createVector(500, 600);
+  shape4g[3] = createVector(450, 675);
+
+  // Define initial position for the blue diamond
+  shape4b[0] = createVector(400, 700);
+  shape4b[1] = createVector(450, 625);
+  shape4b[2] = createVector(500, 700);
+  shape4b[3] = createVector(450, 775);
+
+
 }
+
 function setup() {
-  createCanvas(600, 750);
+  createCanvas(1200, 750);
 
   myButton = new Clickable();     //Create button
   myButton.locate(250, 20);        //Position Button
@@ -67,159 +73,139 @@ function setup() {
   alert("Yay!");                  //Show an alert message
   }
 
-  parallelogramColor = (250, 50, 50);
-  parallelogramMatch = false;
-  circleMatch = false;
-
-  triangleMatch = false;
-
-  diamondMatch = false;
-
   fireworks = loadImage("assets/giphy.gif")
 
   frameRate(240);
 }
-  
+
 function draw() {
   background(120);
+
+  // Draw the blue destination area
+  fill(103, 64, 240);
+  rect(600, 0, 600, 500);
   
-  // Draw the destination parallelogram
-  beginShape(); 
-  fill(150, 150, 150);
+  // Draw all the green shapes
+  fill(178, 180, 5);
+
+  // Draw the green destination area
+  rect(0, 0, 600, 500)
+
+  // Draw the green parallelogram
+  beginShape();
+  for (const { x, y } of shape1g)  vertex(x, y);
+  endShape(CLOSE);
+
+
+
+  circle(circleXg, circleYg, 125);
+
+  // Draw the green triangle
+  beginShape();
+  for (const { x, y } of shape3g)  vertex(x, y);
+  endShape(CLOSE);
+
+  // Draw the green diamond
+  beginShape();
+  for (const { x, y } of shape4g)  vertex(x, y);
+  endShape(CLOSE);
+
+  // Draw all the blue shapes
+  fill(103, 64, 240);
+
+  // Draw the blue parallelogram
+  beginShape();
   for (const { x, y } of shape1b)  vertex(x, y);
   endShape(CLOSE);
-  
-  // Draw the movable parallelogram
+
+  // Draw the blue circle
+  circle(circleXb, circleYb, 125);
+
+  // Draw the blue triangle
   beginShape();
-  if (parallelogramMatch == false) {
-    fill(250, 50, 50);
-  } else {
-    fill(50, 250, 50);
-  }
-  for (const { x, y } of shape1a)  vertex(x, y);
-  endShape(CLOSE);
-
-  
-
-  hitParallelogram = collidePointPoly(mouseX, mouseY, shape1a);
-  //hit = collidePointPoint(100, 100, 100, 100);
-  
-
-  // Move the Paralleleogram with the mouse
-  if (hitParallelogram && mouseIsPressed) {
-    shape1a[0].set(mouseX -75, mouseY + 50);
-    shape1a[1].set(mouseX - 25, mouseY - 50);
-    shape1a[2].set(mouseX + 75, mouseY - 50);
-    shape1a[3].set(mouseX + 25, mouseY + 50);
-  }
-  
-  if (p5.Vector.sub(shape1a[0], shape1b[0]).mag() < 10 &&
-      p5.Vector.sub(shape1a[1], shape1b[1]).mag() < 10 &&
-      p5.Vector.sub(shape1a[2], shape1b[2]).mag() < 10 &&
-      p5.Vector.sub(shape1a[3], shape1b[3]).mag() < 10) {
-      parallelogramMatch = true;
-  }
-
-
-  hitCircle = collidePointCircle(mouseX, mouseY, circleX, circleY, 125);
-  
-  // Draw the destination circle
-  fill(150, 150, 150);
-  circle(500, 600, 125);
-  
-  // Draw the movable circle
-  if (circleMatch == false) {
-    fill (250, 50, 50);
-  }
-  else {
-    fill (50, 250, 50);
-  }
-  
-  circle(circleX, circleY, 125);
-
-  
-
-  if (hitCircle && mouseIsPressed) {
-    circleX = mouseX;
-    circleY = mouseY;
-  }
-
-  circleMatch = collideCircleCircle(500, 600, 25, circleX, circleY, 25);
-
-  // Draw the destination triangle
-  beginShape();
-  fill(150, 150, 150);
   for (const { x, y } of shape3b)  vertex(x, y);
   endShape(CLOSE);
 
-  
-  // Draw the movable triangle
+  // Draw the blue diamond
   beginShape();
-  if (triangleMatch == false) {
-    fill(250, 50, 50);
-  } else {
-    fill(50, 250, 50);
-  }
-  for (const { x, y } of shape3a)  vertex(x, y);
-  endShape(CLOSE);
-  
-  hitTriangle = collidePointPoly(mouseX, mouseY, shape3a);
-  if (hitTriangle && mouseIsPressed) {
-    shape3a[0].set(mouseX - 50, mouseY + 50);
-    shape3a[1].set(mouseX, mouseY - 50);
-    shape3a[2].set(mouseX + 50, mouseY + 50);
-  }
-
-
-  if (p5.Vector.sub(shape3a[0], shape3b[0]).mag() < 10 &&
-      p5.Vector.sub(shape3a[1], shape3b[1]).mag() < 10 &&
-      p5.Vector.sub(shape3a[2], shape3b[2]).mag() < 10) {
-      triangleMatch  = true;
-  }
-
-
-  // Draw the destination diamond
-  beginShape();
-  fill(150, 150, 150);
   for (const { x, y } of shape4b)  vertex(x, y);
   endShape(CLOSE);
 
-  // Draw the movable diamond
-  beginShape();
-  if (diamondMatch == false) {
-    fill(250, 50, 50);
-  } else {
-    fill(50, 250, 50);
-  }
-  for (const { x, y } of shape4a)  vertex(x, y);
-  endShape(CLOSE);
 
-  hitDiamond = collidePointPoly(mouseX, mouseY, shape4a);
 
-  if (hitDiamond && mouseIsPressed) {
-    shape4a[0].set(mouseX - 50, mouseY);
-    shape4a[1].set(mouseX, mouseY - 75);
-    shape4a[2].set(mouseX + 50, mouseY);
-    shape4a[3].set(mouseX, mouseY + 75);
-  }
-
-  if (p5.Vector.sub(shape4a[0], shape4b[0]).mag() < 10 &&
-      p5.Vector.sub(shape4a[1], shape4b[1]).mag() < 10 &&
-      p5.Vector.sub(shape4a[2], shape4b[2]).mag() < 10 &&
-      p5.Vector.sub(shape4a[3], shape4b[3]).mag() < 10) {
-      diamondMatch = true;
-  }
-
-  if (circleMatch && parallelogramMatch && triangleMatch && diamondMatch) {
-    image(fireworks, 80, 100);
+  // Check if the mouse is over the parallelogram
+  hitParallelogramGreen = collidePointPoly(mouseX, mouseY, shape1g);
+  // Move the Paralleleogram with the mouse
+  if (hitParallelogramGreen && mouseIsPressed) {
+    shape1g[0].set(mouseX -75, mouseY + 50);
+    shape1g[1].set(mouseX - 25, mouseY - 50);
+    shape1g[2].set(mouseX + 75, mouseY - 50);
+    shape1g[3].set(mouseX + 25, mouseY + 50);
   }
   
+  // Check if the mouse is hovering over the green circle
+  hitCircleGreen = collidePointCircle(mouseX, mouseY, circleXg, circleYg, 125);
+  // Move the circle with the mouse
+  if (hitCircleGreen && mouseIsPressed) {
+    circleXg = mouseX;
+    circleYg = mouseY;
+  }
 
-  myButton.draw();  
-  
+  // Check if the mouse is over the green triangle
+  hitTriangleGreen = collidePointPoly(mouseX, mouseY, shape3g);
+  // Move the triangle with the mouse
+  if (hitTriangleGreen && mouseIsPressed) {
+    shape3g[0].set(mouseX - 50, mouseY + 50);
+    shape3g[1].set(mouseX, mouseY - 50);
+    shape3g[2].set(mouseX + 50, mouseY + 50);
+  }
 
-  fill(250,250,250);
-  circle(mouseX, mouseY, 10);
+  // Check if the mouse is over the green diamond
+  hitDiamondGreen = collidePointPoly(mouseX, mouseY, shape4g);
+  // Move the diamond with the mouse
+  if (hitDiamondGreen && mouseIsPressed) {
+    shape4g[0].set(mouseX - 50, mouseY);
+    shape4g[1].set(mouseX, mouseY - 75);
+    shape4g[2].set(mouseX + 50, mouseY);
+    shape4g[3].set(mouseX, mouseY + 75);
+  }
+
+
+  // Check if the mouse is over the blue parallelogram
+  hitParallelogramBlue = collidePointPoly(mouseX, mouseY, shape1b);
+  // Move the Paralleleogram with the mouse
+  if (hitParallelogramBlue && mouseIsPressed) {
+    shape1b[0].set(mouseX -75, mouseY + 50);
+    shape1b[1].set(mouseX - 25, mouseY - 50);
+    shape1b[2].set(mouseX + 75, mouseY - 50);
+    shape1b[3].set(mouseX + 25, mouseY + 50);
+  }
+
+  // Check if the mouse is hovering over the blue circle
+  hitCircleBlue = collidePointCircle(mouseX, mouseY, circleXb, circleYb, 125);
+  // Move the circle with the mouse
+  if (hitCircleBlue && mouseIsPressed) {
+    circleXb = mouseX;
+    circleYb = mouseY;
+  }
+
+  // Check if the mouse is over the blue triangle
+  hitTriangleBlue = collidePointPoly(mouseX, mouseY, shape3b);
+  // Move the triangle with the mouse
+  if (hitTriangleBlue && mouseIsPressed) {
+    shape3b[0].set(mouseX - 50, mouseY + 50);
+    shape3b[1].set(mouseX, mouseY - 50);
+    shape3b[2].set(mouseX + 50, mouseY + 50);
+  }
+
+  // Check if the mouse is over the blue diamond
+  hitDiamondBlue = collidePointPoly(mouseX, mouseY, shape4b);
+  // Move the diamond with the mouse
+  if (hitDiamondBlue && mouseIsPressed) {
+    shape4b[0].set(mouseX - 50, mouseY);
+    shape4b[1].set(mouseX, mouseY - 75);
+    shape4b[2].set(mouseX + 50, mouseY);
+    shape4b[3].set(mouseX, mouseY + 75);
+  }
+
 }
-
-
