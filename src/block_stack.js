59f79ed6624e10478f6b2424d68cca297c6ef584
AwesomeDemoGuy <sebastian11ashkar@protonmail.com>
2023-11-13 18:55:40 -0700

    added 2nd level

--- src/block_stack.js
+++ src/block_stack.js
@@ -27,7 +27,6 @@ l8a = [];
 
 l9a = [];
 
-l10a = [];
 function preload() {
 // Level 1 blocks
 // left most block
@@ -36,8 +35,8 @@ function preload() {
 
 direction = "right";
 
-l1_top = 675;
-l1_bottom = 750;
+l1_top = 75;
+l1_bottom = 150;
 
 l1a_left = 226;
 // this is the furtest right line
@@ -70,28 +69,167 @@ l1d[1] = createVector(l1d_left, l1_bottom);
 l1d[2] = createVector(l1d_left, l1_top);
 l1d[3] = createVector(l1d_right, l1_top);
 
+l2_top = 11150;
+l2_bottom = 11225;
+
+l2a_left = 151;
+// This is the furtest right line
+l2a_right = 226;
+l2a[0] = createVector(l2a_right, l2_bottom);
+l2a[1] = createVector(l2a_left, l2_bottom);
+l2a[2] = createVector(l2a_left, l2_top);
+l2a[3] = createVector(l2a_right, l2_top);
+
+l2b_left = 76;
+l2b_right = 151;
+l2b[0] = createVector(l2b_right, l2_bottom);
+l2b[1] = createVector(l2b_left, l2_bottom);
+l2b[2] = createVector(l2b_left, l2_top);
+l2b[3] = createVector(l2b_right, l2_top);
+
+// This is the furthest left line
+l2c_left = 1;
+l2c_right = 76;
+l2c[0] = createVector(l2c_right, l2_bottom);
+l2c[1] = createVector(l2c_left, l2_bottom);
+l2c[2] = createVector(l2c_left, l2_top);
+l2c[3] = createVector(l2c_right, l2_top);
+
+
+level = 1;
+dropping = false;
+
 
 }
 
+
 function setup() {
-  createCanvas(900, 750);
+    createCanvas(900, 750);
 
-  myButton = new Clickable();     //Create button
-  myButton.locate(550, 120);        //Position Button
-  myButton.text = "Return to\nmain menu";    //Set the text displayed in the button
-  myButton.onPress = function(){  //When myButton is pressed
-    this.color = "#AAAAFF";         //Change button color
-    window.close();                  //Close the window since the main manu is still open in the previus tab
-  }
+    myButton = new Clickable();     //Create button
+    myButton.locate(550, 120);        //Position Button
+    myButton.text = "Return to\nmain menu";    //Set the text displayed in the button
+    myButton.onPress = function(){  //When myButton is pressed
+        this.color = "#AAAAFF";         //Change button color
+        window.close();                  //Close the window since the main manu is still open in the previus tab
+    }
 
-  fireworks = loadImage("/assets/giphy.gif");
+    fireworks = loadImage("/assets/giphy.gif");
 
-  frameRate(500);
+    frameRate(500);
+
+    direction = "right";
+
+
 }
 
 function draw() {
     background(120);
 
+    actualDraw();
+    if (level == 1) {
+        direction = check_wall_collision(l1d[1].array()[0], l1a[0].array()[0], direction);
+    } else if (level == 2) {
+        direction = check_wall_collision(l2c[1].array()[0], l2a[0].array()[0], direction);
+    }
+    
+    if (level == 1) {
+        move(direction, 10, l1a);
+        move(direction, 10, l1b);
+        move(direction, 10, l1c);
+        move(direction, 10, l1d);
+    } else if (level == 2) {
+        move(direction, 10, l2a);
+        move(direction, 10, l2b);
+        move(direction, 10, l2c);
+    }
+
+    //console.log(level); 
+
+    if (dropping == true) {
+        if (level - 1 == 1) {
+            if (l1a[0].array()[1] < 750) {
+                console.log("dropping");
+                l1a = drop(l1a);
+                l1b = drop(l1b);
+                l1c = drop(l1c);
+                l1d = drop(l1d);
+            } else {
+                dropping = false;
+            }
+        } else if (level - 1 == 2) {
+            drop(l2a);
+            drop(l2b);
+            drop(l2c);
+        }
+    }
+
+
+}
+
+function move(direction, speed, vector) {
+    if (direction == "left") {
+        vector[0].sub(speed, 0);
+        vector[1].sub(speed, 0);
+        vector[2].sub(speed, 0);
+        vector[3].sub(speed, 0);
+        return;
+    } 
+    else if (direction == "right") {
+        vector[0].sub(-1 * speed, 0);
+        vector[1].sub(-1 * speed, 0);
+        vector[2].sub(-1 * speed, 0);
+        vector[3].sub(-1 * speed, 0);
+        return;
+    }
+}
+
+function check_wall_collision(left, right, direction) {
+    if (left <= 0) {
+        return "right";
+    }
+    else if (right >= 900) {
+        return "left";
+    }
+    return direction
+}
+
+function keyPressed() {
+    if (keyCode === ENTER) {
+        level += 1;
+        dropping = true;
+    }
+    if (level == 2) {
+        console.log(l2a);
+        spawnBlock(11075, l2a);
+        console.log(l2a);
+        spawnBlock(11075, l2b);
+        spawnBlock(11075, l2c);
+    }
+}
+
+function drop(block) {
+    for (let i = 0; i < block.length; i++) { 
+        block[i] = block[i].sub(0, -8);
+    }
+    return block;
+}
+
+
+function moveLevel2(vector) {
+    if (level == 2) {
+        vector[0].sub(0, 2000);
+    }
+}
+
+function spawnBlock(amount, block) {
+    for (let i = 0; i < block.length; i++) {
+        block[i] = block[i].sub(0, amount);
+    }
+}
+
+
+function actualDraw() {
     fill(103, 64, 240);
     beginShape();
     for (const { x, y } of l1a)  vertex(x, y);
@@ -109,32 +247,17 @@ function draw() {
     for (const { x, y } of l1d)  vertex(x, y);
     endShape(CLOSE);
 
-    direction = check_wall_collision(l1d_left, l1a_right);
+    beginShape();
+    for (const { x, y } of l2a)  vertex(x, y);
+    endShape(CLOSE);
 
-    move(direction, 10, l1a_left, l1a_right);
-    move(direction, 10, l1b_left, l1b_right);
-    move(direction, 10, l1c_left, l1c_right);
-    move(direction, 10, l1d_left, l1d_right);
+    beginShape();
+    for (const { x, y } of l2b)  vertex(x, y);
+    endShape(CLOSE);
 
-
+    beginShape();
+    for (const { x, y } of l2c)  vertex(x, y);
+    endShape(CLOSE);
 }
 
-function move(direction, speed, left, right) {
-    if (direction == "left") {
-        left -= speed;
-        right -= speed;
-    } 
-    else if (direction == "right") {
-        left += speed;
-        right += speed;
-    }
-}
 
-function check_wall_collision(left, right) {
-    if (left <= 0) {
-        return "right"
-    }
-    else if (right >= 900) {
-        return "left"
-    }
-}
